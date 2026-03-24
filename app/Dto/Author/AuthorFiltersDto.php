cf8314cd7e1f3a1ca430344f384f1296c56adaf3
Fenix_ITD <starvik100@mail.ru>
2026-03-24 16:26:26 +0300

    Ready to merge! 99%

--- app/Dto/Author/AuthorFiltersDto.php
+++ app/Dto/Author/AuthorFiltersDto.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Dto\Author;
 
+use App\Http\Requests\Author\AuthorListRequest;
+
 final readonly class AuthorFiltersDto
 {
     public function __construct(
@@ -12,4 +14,14 @@ final readonly class AuthorFiltersDto
         public string $sortBy = 'id',
         public string $sortDirection = 'asc',
     ) {}
+
+    public static function fromRequest(AuthorListRequest $request): self
+    {
+        return new self(
+            search: $request->input('search'),
+            perPage: $request->integer('per_page', 15),
+            sortBy: (string) $request->string('sort_by', 'id'),
+            sortDirection: (string) $request->string('sort_direction', 'asc'),
+        );
+    }
 }
