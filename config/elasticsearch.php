a183732a36bf22fe3bd1360fdfe796bba0f9c9ce
Victor <starvik100@mail.ru>
2026-05-21 17:13:01 +0300

    --

--- config/elasticsearch.php
+++ config/elasticsearch.php
@@ -1,6 +1,8 @@
 <?php
 
 return [
+    'host' => env('ELASTICSEARCH_HOST', 'http://localhost:9200'),
+
     'books_index'   => env('ELASTICSEARCH_BOOKS_INDEX', 'books'),
     'authors_index' => env('ELASTICSEARCH_AUTHORS_INDEX', 'authors'),
     'genres_index'  => env('ELASTICSEARCH_GENRES_INDEX', 'genres'),
