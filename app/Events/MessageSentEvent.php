b249fb67346a3fca1504ca7c826559d62ffb9105
Victor <starvik100@mail.ru>
2026-06-18 12:52:36 +0300

    fixed chats

--- app/Events/MessageSentEvent.php
+++ app/Events/MessageSentEvent.php
@@ -9,10 +9,11 @@ use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-final class MessageSentEvent implements ShouldBroadcast
+final class MessageSentEvent implements ShouldBroadcastNow
 {
     use Dispatchable;
     use InteractsWithSockets;
