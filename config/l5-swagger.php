48672703cd4ee67ee9bdf32aad9f4da4e9565a22
Victor <starvik100@mail.ru>
2026-08-24 17:17:31 +0300

    10 OK

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -65,9 +65,9 @@ return [
              * Middleware allows to prevent unexpected access to API documentation
              */
             'middleware' => [
-                'api' => [],
-                'asset' => [],
-                'docs' => [],
+                'api' => ['auth', 'admin'],
+                'asset' => ['auth', 'admin'],
+                'docs' => ['auth', 'admin'],
                 'oauth2_callback' => [],
             ],
 
